e7aab8a5c452cbe0e709b7c6548d8418cafe7f56
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-03-22 19:06:01 +0530

    Model Change

--- src/Models/user.model.js
+++ src/Models/user.model.js
@@ -16,10 +16,6 @@ var userSchema = new mongoose.Schema(
       unique: true,
       required: true,
     },
-    phone: {
-      type: Number,
-      required: false,
-    },
     password: {
       type: String,
       required: true,
